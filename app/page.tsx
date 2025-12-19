42d16e7d3a3754f046001642d8557d02d137f595
neutroufromfortytwo <itsneutrouthetrou@gmail.com>
2025-12-19 14:23:10 +0100

    reveal animation OK

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,54 @@
 "use client"
 
+import { motion, easeOut } from "framer-motion"
 import Button from "./components/Button"
+
+const initialReveal = {
+  scaleX: 0,
+  originX: 0.5,
+  originY: 0.5,
+};
+
+
+const animateRevealA = {
+  scaleX: [0, 1, 1],
+  height: [0, 0, 470],
+  borderColor: ["#fff", "#fff", "#fff0"],
+};
+const animateRevealB = {
+  scaleX: [0, 1, 1, 1],
+  height: [0, 0, 470, 470],
+  borderColor: ["#fff", "#fff", "#fff", "#fff0"],
+};
+
+/*choose between A and B*/
+const animateReveal = animateRevealB;
+
+const transitionReveal = {
+  duration: 1.5,
+  ease: easeOut,
+  delay: 0.4
+};
 
 export default function Home() {
   return (
-    <div
-      className="h-470 w-432 flex flex-col gap-50 overflow-hidden"
-    >
-      <Button text="Summary" color="purple"/>
-      <Button text="About me" color="red"/>
-      <Button text="Projects" color="blue"/>
-      <Button text="Tech Stack" color="orange"/>
-    </div>
+    <section>
+      <div className="h-470 w-432 absolute flex flex-col">
+        <div className="bg-(--background) flex-1"/>
+        <motion.div
+          initial={initialReveal}
+          animate={animateReveal}
+          transition={transitionReveal}
+          className="bg-transparent border-solid border-white border-2"
+          />
+        <div className="bg-(--background) flex-1"/>
+      </div>
+      <div className="h-470 w-432 flex flex-col gap-50 overflow-hidden">
+        <Button text="Summary" color="purple"/>
+        <Button text="About me" color="red"/>
+        <Button text="Projects" color="blue"/>
+        <Button text="Tech Stack" color="orange"/>
+      </div>
+    </section>
   );
 }
