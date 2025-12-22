00c0337706917893cb9c5ab5e7dcaec7a06a32bd
Forty <fortytwools@gmail.com>
2025-12-22 23:50:44 +0100

    landing page animation OK

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {SummaryButton, AboutButton, ProjectsButton, StackButton} from "./components/Button"
 import { motion, easeOut } from "framer-motion"
 
 export default function Home() {
   const [revealed, setRevealed] = useState(false);
+  const height = "calc(var(--spacing) * 90)";
+
   const initialReveal = {
     scaleX: 0,
     originX: 0.5,
@@ -13,12 +15,12 @@ export default function Home() {
   };
   const animateRevealA = {
     scaleX: [0, 1, 1],
-    height: [0, 0, 60],
+    height: [0, 0, height],
     borderColor: ["#fff", "#fff", "#fff0"],
   };
   const animateRevealB = {
     scaleX: [0, 1, 1, 1],
-    height: [0, 0, 60, 60],
+    height: [0, 0, height, height],
     borderColor: ["#fff", "#fff", "#fff", "#fff0"],
   };
   /*choose between A and B*/
@@ -31,17 +33,17 @@ export default function Home() {
 
   return (
     <section className="flex-1 flex items-center justify-center">
-      {/* <div className={`h-60 w-70 absolute flex flex-col ${revealed ? "pointer-events-none" : "pointer-events-auto"}`}> */}
-        {/* <div className="bg-(--background) flex-1"/> */}
-          {/* <motion.div
+      <div className={`h-90 w-70 absolute flex flex-col ${revealed ? "pointer-events-none" : "pointer-events-auto"}`}>
+        <div className="bg-(--background) flex-1"/>
+          <motion.div
             initial={initialReveal}
             animate={animateReveal}
             transition={transitionReveal}
             onAnimationComplete={() => setRevealed(true)}
             className="bg-transparent border-texture"
-          /> */}
-          {/* <div className="bg-(--background) flex-1"/> */}
-      {/* </div> */}
+          />
+          <div className="bg-(--background) flex-1"/>
+      </div>
       <div className="h-90 w-70 flex flex-col justify-between">
         <SummaryButton/>
         <AboutButton/>
