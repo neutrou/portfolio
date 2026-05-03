3b1bb1ab8c89c26a63707488b865c24b08f89e89
linuxNeutrou <itsneutrouthetrou@gmail.com>
2026-05-04 00:45:37 +0200

    added 404 pages

---
+++ app/projects/[slug]/not-found.tsx
@@ -0,0 +1,13 @@
+import Separator from "@/app/components/Separator"
+
+export default function NotFound() {
+  return (
+    <div className="size-full flex items-center justify-center">
+      <div className="flex flex-row gap-2.5 items-center justify-center h-20 text-lg text-text-primary">
+        <h1>404</h1>
+        <Separator className="w-0.5 h-full" orientation="vertical" />
+        <h1>Project Not Found</h1>
+      </div>
+    </div>
+    );
+}
