4ddf96f0cc364646c0ee030a438ec5e21429fbf8
linuxNeutrou <itsneutrouthetrou@gmail.com>
2026-05-04 00:45:30 +0200

    added horizontal mode for carousel

--- app/components/Separator.tsx
+++ app/components/Separator.tsx
@@ -1,9 +1,17 @@
 interface SeparatorProps {
   className?: string;
+  orientation?: 'horizontal' | 'vertical';
 }
 
-export default function Separator({ className }: SeparatorProps) {
-  return (
-    <hr className={`h-0.5 w-full bg-[radial-gradient(50%_50%_at_50%_50%,#F7F5FB_0%,rgba(115,115,115,0)_100%)] border-0 ${className ?? ''}`} />
-  );
+export default function Separator({
+  className = '',
+  orientation = 'horizontal',
+}: SeparatorProps) {
+  const baseClasses = 'border-0';
+  const orientationClasses =
+    orientation === 'vertical'
+      ? 'h-full w-0.5 bg-[radial-gradient(50%_50%_at_50%_50%,#F7F5FB_0%,rgba(115,115,115,0)_100%)]'
+      : 'h-0.5 w-full bg-[radial-gradient(50%_50%_at_50%_50%,#F7F5FB_0%,rgba(115,115,115,0)_100%)]';
+
+  return <hr className={`${baseClasses} ${orientationClasses} ${className}`} />;
 }
